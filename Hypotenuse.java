56f3a99b6e59035901d20ea7296f02f535ddd8ce
NTA-Drooid <ntadroid@gmail.com>
2024-08-26 20:38:15 +0400

    Hypotenuse.java

---
+++ Hypotenuse.java
@@ -0,0 +1,42 @@
+public class Hypotenuse {
+    public static void main(String[] args) {
+
+        //Hypotenuse function
+
+        //first need is function of scanner to take an input then expression
+        double x;
+        double y;
+        double z;
+//math.pow(5, 2) (number, root)
+
+
+        //z = sqrt()
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Enter side x: ");
+//        x = scanner.nextDouble();
+//
+//
+//        System.out.println("Enter side y: ");
+//        y = scanner.nextDouble();
+//
+//        z = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+//
+//        System.out.println("Hypotenuos is "+ z);
+//
+
+
+
+//        Double sidex =Double.parseDouble( JOptionPane.showInputDialog("Enter x: "));
+//        JOptionPane.showMessageDialog(null, "x is "+ sidex);
+//
+//        Double sidey = Double.parseDouble(  JOptionPane.showInputDialog("Enter your sidey") );
+//        JOptionPane.showMessageDialog(null, "y is "+sidey);
+//
+//        z = Math.sqrt(Math.pow(sidex, 2) + Math.pow(sidey, 2));
+//        JOptionPane.showInternalMessageDialog(null, " Hypotrnuos is " + z);
+
+    }
+
+}
